0c6018990ed1e17047fadb5c95e070788dc99f2e
roicebaal <shar0939@algonquinlive.com>
2025-05-13 13:21:16 -0400

    Create index.js

---
+++ JS/index.js
@@ -0,0 +1,38 @@
+//Task 1
+alert("First Exercise run");
+let a = 3;
+              let b = 5;
+              let c;
+
+let output = ` let a = 3;
+              let b = 5;
+              let c;
+              a + b = ${a + b}
+              a - b = ${a - b}
+              a * b = ${a * b}
+              a / b = ${a / b}
+              a % b = ${a % b}
+             (a += b) = ${a += b}
+             (a -= b) = ${a -= b}
+             (a *= b) = ${a *= b}
+             (a /= b) = ${a /= b}
+             (a %= b) = ${a %= b}
+             a == b = ${a == b}
+             a != b = ${a != b}
+             a > b = ${a > b}
+             a < b = ${a < b}
+            !a && !c = ${!a && !c}
+            !a || !c = ${!a || !c}`
+
+console.log(output)
+
+
+// Task 2
+
+let fn = `Bharat`
+let ln = `Sharma`
+let email = `sharmabharat@assignment1.com`
+
+let information = `Hi there! My name is ${fn}${ln} and you can contact me at ${email}`
+
+console.log(information)
